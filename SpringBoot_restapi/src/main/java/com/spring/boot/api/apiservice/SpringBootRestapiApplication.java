6e8011536ebfc1f9fc724c515845a0d2a46537dd
px2083 <px2083@gmail.com>
2020-02-05 22:01:39 +0900

    move to main from test

--- SpringBoot_restapi/src/main/java/com/spring/boot/api/apiservice/SpringBootRestapiApplication.java
+++ SpringBoot_restapi/src/main/java/com/spring/boot/api/apiservice/SpringBootRestapiApplication.java
@@ -2,12 +2,22 @@ package com.spring.boot.api.apiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@RequestMapping("/restapi")
 public class SpringBootRestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestapiApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String HelloWorld() {
+		return "Hello World";
 	}
 
 }
