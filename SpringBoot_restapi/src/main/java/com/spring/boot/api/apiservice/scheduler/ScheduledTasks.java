3ed877f01697d69ca92216e04d9a33735e9068f7
px2083 <px2083@gmail.com>
2020-02-13 23:02:04 +0900

    kafka send receive test, use scheduler

--- SpringBoot_restapi/src/main/java/com/spring/boot/api/apiservice/scheduler/ScheduledTasks.java
+++ SpringBoot_restapi/src/main/java/com/spring/boot/api/apiservice/scheduler/ScheduledTasks.java
@@ -3,7 +3,6 @@ package com.spring.boot.api.apiservice.scheduler;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,30 +14,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledTasks {
 
-    private static final Logger log = LogManager.getLogger(ScheduledTasks.class);
+	private static Logger log = LogManager.getLogger(ScheduledTasks.class);
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public void send(String topic, String payload) {
-        kafkaTemplate.send(topic, payload);
-        log.info("Message: " + payload + " sent to topic: " + topic);
-        System.out.println("Message: " + payload + " sent to topic: " + topic);
-    }
+	
+	public void send(String topic, String payload) { 
+		kafkaTemplate.send(topic, payload); 
+		log.info("Message: " + payload + " sent to topic: " + topic); 
+	}
+	
+	
+	@Scheduled(fixedRate = 1000) 
+	public void reportCurrentTime() {
+		send("test", "helloworld " + dateFormat.format(new Date())); 
+	}
+	
 
-
-    @Scheduled(fixedRate = 1000)
-    public void reportCurrentTime() {
-        System.out.println("helloworld " + dateFormat.format(new Date()));
-        log.info("helloworld " + dateFormat.format(new Date()));
-        // send("test", "helloworld " + dateFormat.format(new Date()));
-    }
-
-    @KafkaListener(topics = "test")
-    public void receiveTopic1(ConsumerRecord consumerRecord) {
-        log.info("Receiver on topic1: "+consumerRecord.toString());
-        System.out.println("Receiver on topic1: "+consumerRecord.toString());
+	@KafkaListener(topics = "test", groupId = "group-id")
+    public void receiveTopic1(String message) {
+        log.info("Receiver on topic1: "+message);
     }
 }
